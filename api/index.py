56c1123fff8fe3d0c32e03c7ef710c0b123d0b93
Ward Weistra <ward@fire.ly>
2023-11-09 16:44:03 +0100

    Catch all route

--- api/index.py
+++ api/index.py
@@ -4,4 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    return f'Welcome to our URL redirector!'
+
+@app.route('/')
+@app.route('/<first>')
+@app.route('/<first>/<path:rest>')
+def fallback(first=None, rest=None):
     return f'The current URL is: {request.url}'
