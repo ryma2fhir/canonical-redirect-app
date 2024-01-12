6ec89d1c00a3459d8f200be6ad482661bbd83042
Ryan May <ryan.may2@nhs.net>
2024-01-12 11:06:42 +0000

    added redirects for R4 and STU3

--- api/index.py
+++ api/index.py
@@ -12,14 +12,29 @@ def home():
                            example_indirect_canonical=example_indirect_canonical,
                            example_direct_canonical=example_direct_canonical)
 
+''' Redirects R4 version of UK Core '''
 @app.route('/')
-@app.route('/<path:rest>')
+@app.route('/CapabilityStatement/<path:rest>')
+@app.route('/CodeSystem/<path:rest>')
+@app.route('/ConceptMap/<path:rest>')
+@app.route('/StructureDefinition/<path:rest>')
+@app.route('/ValueSet/<path:rest>')
 def canonicalRedirectDirect(rest=None):
     canonical = str(request.url)
     scope = "hl7fhirukcorer4@current"
     redirect_url = resolve_url.format(canonical, scope)
     return redirect(redirect_url, code=301)
+ 
+''' Redirects STU3 version of UK Core ''' 
+@app.route('/')
+@app.route('/STU3/<path:rest>')
+def canonicalRedirectDirect(rest=None):
+    canonical = str(request.url)
+    scope = "careconnect.stu3.03.00.00@3.1.0"
+    redirect_url = resolve_url.format(canonical, scope)
+    return redirect(redirect_url, code=301)
 
+'''
 @app.route('/')
 @app.route('/<path:rest>')
 def canonicalRedirectIndirect(rest=None):
@@ -27,7 +42,8 @@ def canonicalRedirectIndirect(rest=None):
     scope = "hl7fhirukcorer4"
     redirect_url = resolve_url.format(canonical, scope)
     return render_template('redirect.html', redirect_url=redirect_url)
-    
+'''    
+
 @app.route('/<path:rest>')
 def Fallback(rest=None):
     return render_template('other.html')
